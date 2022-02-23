9d7abef571a87b47339af510d6b314ee7bf80cfc
Dawid Konrad Kohnke <dawid.kohnke.cad@gmail.com>
2022-02-23 17:44:43 +0100

    working calculator

---
+++ test_calculator.py
@@ -0,0 +1,18 @@
+from calculator import Calculator
+import itertools
+
+calc = Calculator()
+
+def test_calculator():
+    calc = Calculator()
+    assert calc.add(5.0)==5.0
+    assert calc.add(10.0)==15.0
+    assert calc.subtract(10.0)==0.0
+    assert calc.multiply(5)==0.0
+    assert calc.add(0.1)==0.1
+    assert calc.add(0.2)==0.3
+    assert calc.get_state()==0.3
+    assert calc.reset()==0
+    assert calc.subtract(-15.00)==15.0
+    assert calc.divide(3.0)==5.0
+    assert calc.multiply(-3.0)==-15.0
